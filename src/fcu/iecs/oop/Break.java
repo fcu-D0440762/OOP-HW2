2d505463933f7d5133851b86a6c1aab77ac0bfde
blue <blue19970731@gmail.com>
2016-10-18 21:43:36 +0800

    hw3

---
+++ src/fcu/iecs/oop/Break.java
@@ -0,0 +1,15 @@
+package fcu.iecs.oop;
+
+public class Break {
+
+		public static void main(String[] args) {
+			
+			for(int i = 0; i < 5; i ++) {
+				for(i = 0 ; i < 2; i ++){
+					System.out.println(i);
+				}
+				break;
+			}
+	}
+
+}
--- src/fcu/iecs/oop/StringComparator.java
+++ src/fcu/iecs/oop/StringComparator.java
@@ -5,7 +5,8 @@ public class StringComparator {
 	public static void main(String[] args) {
 		
 		String str1, str2 ;
-		
+		boolean index;
+		do{
 		System.out.println("Enter a string 1 :") ;
 		Scanner scanner1 = new Scanner(System.in);
 		str1 = scanner1.nextLine();
@@ -15,14 +16,8 @@ public class StringComparator {
 		str2 = scanner2.nextLine() ;
 		
 		
-		
-		if((str1.toUpperCase()).equals(str2.toUpperCase())){
-			System.out.println("The two strings are the same.") ;
-			
-		}else{
-			System.out.println("The two strings are not the same.") ;
-		}
-
+		index = !(str1.toUpperCase()).equals(str2.toUpperCase());
+		}while(index);
 		
 		
 		
